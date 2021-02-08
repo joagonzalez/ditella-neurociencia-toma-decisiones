a0a115cc5e61136f610dab9cdf3aa8d6c592b24e
Joaquin Gonzalez <joagonzalez@gmail.com>
2021-02-08 20:42:49 -0300

    python template

---
+++ src/tp1/analisis.py
@@ -0,0 +1,17 @@
+import os
+import sys
+
+
+class DataAnalysis:
+    def __init__(self):
+        pass
+
+    def get_results(self):
+        pass
+
+    def read_file(filename, dir=None):
+        pass
+
+
+if __name__ == __main__:
+    pass
