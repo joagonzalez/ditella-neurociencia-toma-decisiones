4362d469457143a263fd4905a39ee7b29555ffba
Joaquin Gonzalez <joagonzalez@gmail.com>
2021-02-10 17:44:53 -0300

    multifile refactor and only right answers

--- src/tp1/analisis.py
+++ src/tp1/analisis.py
@@ -54,106 +54,114 @@ class DataAnalysis:
 
         self.FILES = [f for f in listdir(directory) if isfile(join(directory, f))]
 
-    def create_result_structure(self, language):
+    def create_result_structure(self, filename, language):
         '''
         Adds generic result structure for a new language detected
         '''
-        self.RESULTS[language] = {}
+        print(f'Creating data structure for {filename}-{language}')
+
+        if filename not in self.RESULTS:
+            self.RESULTS[filename] = {}
+
+        if language not in self.RESULTS[filename]:
+            self.RESULTS[filename][language] = {}
+
         if language == '0':
-            self.RESULTS[language]['language'] = 'ingles'
+            self.RESULTS[filename][language]['language'] = 'ingles'
         else:
-            self.RESULTS[language]['language'] = 'español'
-        self.RESULTS[language]['consistent'] = []
-        self.RESULTS[language]['inconsistent'] = []
-        self.RESULTS[language]['difficulty'] = []
-        self.RESULTS[language]['response_time'] = []
-        self.RESULTS[language]['correct_answers'] = []
-        self.RESULTS[language]['muCongruentes'] = 0
-        self.RESULTS[language]['muIncongruentes'] = 0
-        self.RESULTS[language]['stdCongruentes'] = 0
-        self.RESULTS[language]['stdIncongruentes'] = 0
+            self.RESULTS[filename][language]['language'] = 'español'
+        self.RESULTS[filename][language]['consistent'] = []
+        self.RESULTS[filename][language]['inconsistent'] = []
+        self.RESULTS[filename][language]['difficulty'] = []
+        self.RESULTS[filename][language]['response_time'] = []
+        self.RESULTS[filename][language]['correct_answers'] = []
+        self.RESULTS[filename][language]['muCongruentes'] = 0
+        self.RESULTS[filename][language]['muIncongruentes'] = 0
+        self.RESULTS[filename][language]['stdCongruentes'] = 0
+        self.RESULTS[filename][language]['stdIncongruentes'] = 0
 
     def get_list_files(self):
         return self.FILES
 
-    def get_difficulty(self):
-        self.read_list_files()
+    def get_difficulty(self, filename):
 
-        for file in self.FILES:
-            tabla = open(self.DIR + file)
-            i=0
-            while i < self.TRIALS: 
-                line=tabla.readline()
-                parsed_line=line.split()
-                language = parsed_line[4]
-                if language not in self.LANGUAGES:
-                    self.LANGUAGES.append(language)
-                    self.create_result_structure(language)
+        tabla = open(self.DIR + filename)
+        i=0
+        while i < self.TRIALS: 
+            line=tabla.readline()
+            parsed_line=line.split()
+            language = parsed_line[4]
+            
+            if language not in self.LANGUAGES:
+                self.LANGUAGES.append(language)
+            
+            if filename not in self.RESULTS or language not in self.RESULTS[filename]:
+                self.create_result_structure(filename, language)
+            
+            if parsed_line[7] == '1': # me quedo solo con los que conteste bien
+                self.RESULTS[filename][language]['response_time'].append(float(parsed_line[8]))
+                self.RESULTS[filename][language]['correct_answers'].append(parsed_line[7])
+                self.RESULTS[filename][language]['difficulty'].append(int(parsed_line[3]))
+            
+            i+=1
+        
+        # print(self.RESULTS[filename])
 
-                    self.RESULTS[language]['response_time'].append(float(parsed_line[8]))
-                    self.RESULTS[language]['correct_answers'].append(parsed_line[7])
-                    self.RESULTS[language]['difficulty'].append(int(parsed_line[3]))
-                else:
-                    self.RESULTS[language]['response_time'].append(float(parsed_line[8]))
-                    self.RESULTS[language]['correct_answers'].append(parsed_line[7])
-                    self.RESULTS[language]['difficulty'].append(int(parsed_line[3]))
+        # me quedo solo con los que conteste bien
+        for language, values in self.RESULTS[filename].items():
+            self.RESULTS[filename][language]['consistent'] = [i for i, x in enumerate(self.RESULTS[filename][language]['difficulty']) if x == 1 and self.RESULTS[filename][language]['correct_answers'][i] == '1']
+            self.RESULTS[filename][language]['inconsistent'] = [i for i, x in enumerate(self.RESULTS[filename][language]['difficulty']) if x == 0 and self.RESULTS[filename][language]['correct_answers'][i] == '1']
 
-                i+=1
-            print(self.RESULTS)
-            for language, values in self.RESULTS.items():
-                self.RESULTS[language]['consistent'] = [i for i, x in enumerate(self.RESULTS[language]['difficulty']) if x == 1]
-                self.RESULTS[language]['inconsistent'] = [i for i, x in enumerate(self.RESULTS[language]['difficulty']) if x == 0]
+        # print('english results')
+        # print(self.RESULTS[filename]['0'])
+        # print('spanish results')
+        # print(self.RESULTS[filename]['1'])
 
-            print('english results')
-            print(self.RESULTS['0'])
-            print('spanish results')
-            print(self.RESULTS['1'])
-
-    def analyze_data(self, language):
-        print(f'Analyzing data for {self.RESULTS[language]["language"]}')
-        response_time=np.array(self.RESULTS[language]['response_time'])
+    def analyze_data(self, filename, language):
+        print(f'Analyzing data for {self.RESULTS[filename][language]["language"]}')
+        response_time=np.array(self.RESULTS[filename][language]['response_time'])
         print(response_time)
         
-        self.RESULTS[language]['muCongruentes'] = np.mean(response_time[self.RESULTS[language]['consistent']]);
-        self.RESULTS[language]['muIncongruentes'] = np.mean(response_time[self.RESULTS[language]['inconsistent']]);
-        self.RESULTS[language]['stdCongruentes'] = np.std(response_time[self.RESULTS[language]['consistent']]);
-        self.RESULTS[language]['stdIncongruentes'] = np.std(response_time[self.RESULTS[language]['inconsistent']]);
+        self.RESULTS[filename][language]['muCongruentes'] = np.mean(response_time[self.RESULTS[filename][language]['consistent']]);
+        self.RESULTS[filename][language]['muIncongruentes'] = np.mean(response_time[self.RESULTS[filename][language]['inconsistent']]);
+        self.RESULTS[filename][language]['stdCongruentes'] = np.std(response_time[self.RESULTS[filename][language]['consistent']]);
+        self.RESULTS[filename][language]['stdIncongruentes'] = np.std(response_time[self.RESULTS[filename][language]['inconsistent']]);
 
-        print(f'Mean consistent {self.RESULTS[language]["muCongruentes"]}')
-        print(f'Std consistent {self.RESULTS[language]["stdCongruentes"]}')
-        print(f'Mean inconsistent {self.RESULTS[language]["muIncongruentes"]}')
-        print(f'Std inconsistent{self.RESULTS[language]["stdIncongruentes"]}')
+        print(f'Mean consistent {self.RESULTS[filename][language]["muCongruentes"]}')
+        print(f'Std consistent {self.RESULTS[filename][language]["stdCongruentes"]}')
+        print(f'Mean inconsistent {self.RESULTS[filename][language]["muIncongruentes"]}')
+        print(f'Std inconsistent {self.RESULTS[filename][language]["stdIncongruentes"]}')
 
-    def is_significant(self, language):
-        response_time = np.array(self.RESULTS[language]['response_time'])
-        test=stats.ttest_ind(response_time[self.RESULTS[language]['consistent']], 
-                             response_time[self.RESULTS[language]['inconsistent']]);
+    def is_significant(self, filename, language):
+        response_time = np.array(self.RESULTS[filename][language]['response_time'])
+        test=stats.ttest_ind(response_time[self.RESULTS[filename][language]['consistent']], 
+                             response_time[self.RESULTS[filename][language]['inconsistent']]);
 
         print(f'Is significant for language {language}: {test}')
         
         return(test)
     
-    def plot_cons_incons(self, language):
+    def plot_cons_incons(self, filename, language):
         n_groups = 2
 
-        test = self.is_significant(language)
+        test = self.is_significant(filename, language)
 
         fig, ax = plt.subplots()
 
-        rects1 = ax.bar(1, self.RESULTS[language]["muCongruentes"], 0.3,
+        rects1 = ax.bar(1, self.RESULTS[filename][language]["muCongruentes"], 0.3,
                         alpha=0.4, color='b',
-                        yerr=self.RESULTS[language]['stdCongruentes']/np.sqrt(len(self.RESULTS[language]['consistent'])),
-                        label=f'Congruentes [{self.RESULTS[language]["language"]}]')
+                        yerr=self.RESULTS[filename][language]['stdCongruentes']/np.sqrt(len(self.RESULTS[filename][language]['consistent'])),
+                        label=f'Congruentes [{self.RESULTS[filename][language]["language"]}]')
 
-        rects2 = ax.bar(1 + 0.35, self.RESULTS[language]["muIncongruentes"], 0.3,
+        rects2 = ax.bar(1 + 0.35, self.RESULTS[filename][language]["muIncongruentes"], 0.3,
                         alpha=0.4, color='r',
-                        yerr=self.RESULTS[language]['stdIncongruentes']/np.sqrt(len(self.RESULTS[language]['inconsistent'])),
-                        label=f'Incongruentes [{self.RESULTS[language]["language"]}]')
+                        yerr=self.RESULTS[filename][language]['stdIncongruentes']/np.sqrt(len(self.RESULTS[filename][language]['inconsistent'])),
+                        label=f'Incongruentes [{self.RESULTS[filename][language]["language"]}]')
 
         plt.ylim([600,1600])
         plt.xticks([1,1.35], ('1', '2'))
         ax.set_ylabel('Response Time')
-        ax.set_title('P='+str(test[1]))
+        ax.set_title(f'P={str(test[1])} \nFilename: {filename}')
         ax.legend(loc='upper left')
         plt.show() 
 
@@ -161,11 +169,13 @@ class DataAnalysis:
 
 if __name__ == "__main__":
     informe = DataAnalysis()
+    print(informe.get_list_files())
     for filename in informe.get_list_files():
         print(f'Solving analysis for file: {filename}')
         informe.read_file(filename)
-        informe.get_difficulty()
-        for language in informe.LANGUAGES:
-            informe.analyze_data(str(language))
-            informe.is_significant(str(language))
-            informe.plot_cons_incons(str(language))
+        informe.get_difficulty(filename)
+        for language in informe.RESULTS[filename]:
+            informe.analyze_data(filename, language)
+            informe.is_significant(filename, language)
+            informe.plot_cons_incons(filename, language)
+    print(informe.RESULTS)
