ee2772f86a26356b0b7a197f2c8fbae7dd01145a
Joaquin Gonzalez <joagonzalez@gmail.com>
2021-02-11 00:33:41 -0300

    generic lines in data file

--- src/tp1/analisis.py
+++ src/tp1/analisis.py
@@ -100,10 +100,11 @@ class DataAnalysis:
         '''
         tabla = open(self.DIR + filename)
         i=0
-        while i < self.TRIALS: 
-            line=tabla.readline()
+        line = tabla.readline()
+        while line: 
             parsed_line=line.split()
             language = parsed_line[4]
+            line=tabla.readline()
             
             if language not in self.LANGUAGES:
                 self.LANGUAGES.append(language)
